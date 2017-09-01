139067b66bde7ddd281f67e8433de026338a8316
Javier Santibáñez <jsantibanez@yahoo.com>
2017-09-01 18:23:27 -0500

    Cambios: colores, rango de valores de entrada

--- server.R
+++ server.R
@@ -34,28 +34,25 @@ shinyServer(function(input, output) {
     
     par(mar = c(2, 2.5, 1, 1))
     
-    p <- plot(x,y,axes = F,bg="blue3",pch=21,col="blue")
+    plot(x, y, axes = F, pch = 16, col = 'steelblue', cex = 2)
     axis(1, at = seq(-5, 5, by = 1))
-    axis(2, at = seq(-22, 24, by = 2), las = 2)
-    r <- abline(a = beta0, b = beta1, col = 'red2', lwd = 2)
-    abline(h=0,col="cyan3")
-    abline(v=0,col="cyan3")
-    text(-2, 15, paste0('y = ', paste(beta0),' + ', paste(beta1), 'x, Q = ', re), col = 'red2')
-    text(-2, 10, paste0('b0 = ', paste(b0),' b1= ', paste(b1)), col = 'blue2')
+    axis(2, at = seq(-22, 24, by = 4), las = 2)
+    r <- abline(a = beta0, b = beta1, col = 'red4', lwd = 3)
+    abline(h=0, col = 'darkgray', lwd = 3, lty = 3)
+    abline(v=0, col = 'darkgray', lwd = 3, lty = 3)
+    text(-2, 15, paste0('Q = ', re), col = 'red4', cex = 1.5)
+    text(-2, 10, paste0('b0 = ', paste(b0),'    b1= ', paste(b1)), col = 'steelblue', cex = 1.5)
     box()
   })
   
   output$graph2 <- renderPlot({
-    
-    beta0 <- input$beta0  # Infected
-    beta1  <- input$beta1   # Total Population
+    beta0 <- input$beta0
+    beta1  <- input$beta1
     
     set.seed(1994)
-    # Change in Susceptible
     x  <- round(runif(16, -5, 5), 1)
     b0 <- round(runif(1, -2, 2), 2)
     b1 <- round(runif(1, 1, 5), 2)
-    # Change in Contageous
     y  <- b0 +b1*x + rnorm(16, 0, 2)
     
     q2 <- function(bet0, bet1){
@@ -74,17 +71,17 @@ shinyServer(function(input, output) {
     b1y <- seq(-10, 10, by = 0.1)
     q2z <- q2(b0x, b1y)
     
-    curvas <- c(seq(30, 120, by = 20), seq(100, 300, by = 100), seq(400, 2000, by = 200))
-    
     par(mar = c(2, 2.5, 1, 1))
     
-    image(b0x,b1y,q2z,col=topo.colors(16),breaks = sort(curvas),
-          xlim = c(-7, 8), ylim = c(0,8.5), axes = F)
-    contour(b0x, b1y, q2z, levels = curvas, add = TRUE)
-    axis(1, at = seq(-7, 8, by = 1))
-    axis(2, at = seq(0, 9, by = 1),las=2)
-    box()
-    points(beta0, beta1, pch = 16, col = 'red2')
+    colores <- colorRampPalette(c('steelblue', '#FFF9C4', 'red4'))
+    curvas <- c(seq(0, 200, length.out=5), seq(200, 1000, length.out=5), seq(1000, 6000, length.out=10))
+    
+    library(lattice)
+    image(b0x, b1y, q2z, col = colores(length(curvas)-1), breaks = sort(curvas),
+         xlim = c(-8, 8), ylim = c(0, 8), axes = F, useRaster = T)
+    contour(b0x, b1y, q2z, levels = curvas, add = TRUE, col = 'black', drawlabels = F)
+    points(beta0, beta1, pch = 3, cex = 2, col = 'green2', lwd = 3)
+    axis(1, seq(-8, 8, by = 2))
+    axis(2, seq(0, 8, by = 1), las = 2)
   })
-  
 })
--- ui.R
+++ ui.R
@@ -2,22 +2,24 @@ library(shiny)
 
 
 shinyUI(
-  fluidPage(theme = 'slate.css',
+  fluidPage(theme = 'bootstrap.css',
     titlePanel('Mínimos Cuadrados Ordinarios'),
     sidebarLayout(
       sidebarPanel(
         
-        tags$h3("Estimacion Beta"),
+        tags$h3("Parámetros"),
         
-        sliderInput("beta0", "Beta_0:", 
-                    min=6, max=8, value=7, step=0.1),
+        sliderInput("beta0", "Beta 0:", 
+                    min=-8, max=8, value=0, step=0.1),
         
-        sliderInput("beta1", "Beta_1:", 
-                    min=2, max=4, value=3, step=0.1)
+        sliderInput("beta1", "Beta 1:", 
+                    min=0, max=8, value=1, step=0.1)
         ),
       mainPanel(
-        plotOutput("graph1"),
-        plotOutput("graph2")  
+        tags$h3('Modelo ajustado'),
+        plotOutput('graph1'),
+        tags$h3('Suma de cuadrados'),
+        plotOutput('graph2')  
       )
     )
   )
